a244d18b13fbe63b0367f43dd714e9e3d790dd20
gtfarng <5735512002@psu.ac.th>
2019-09-18 10:30:06 +0700

    example_to_connect_mysql

---
+++ util/env.js
@@ -0,0 +1,15 @@
+const env = {
+  database: 's5735512002_640',
+  username: 's5735512002_640',
+  password: 'MwKNG6jg',
+  host: 'http://student.coe.phuket.psu.ac.th',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
